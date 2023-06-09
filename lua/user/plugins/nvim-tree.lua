9b198e2f81f61d99ba04a5090722ffbbf59ef70b
malcolmwilliams <malcolmnwilliams@gmail.com>
2023-06-09 08:44:48 -0400

    feat: nvimtree open horizontal split with 's'

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -111,6 +111,7 @@ local function on_attach(bufnr)
   vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
   vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
   vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
 
 end
 
